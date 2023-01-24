c323b6bf880468d3866e551cca0f1ff5330a2aa2
arshdeepmay <19bcs1407@gmail.com>
2023-01-25 00:25:22 +0530

    Top loading Bar like youtube added

--- src/Component/News.js
+++ src/Component/News.js
@@ -27,15 +27,19 @@ export default class News extends Component {
     document.title= `News Monkey - ${this.props.category.toUpperCase()}`
   }
   async updateNews(){
+     this.props.setProgress(10);
     let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=271126b49b7c45ddaca3ee43f3937987&page=${this.state.page}&pageSize=${this.props.pageSize}`;
     this.setState({loading:true})
     let data= await fetch(url);
+    this.props.setProgress(30)
     let parsedData=await data.json()
+    this.props.setProgress(60)
     this.setState({
       articles:parsedData.articles,
       totalResults:parsedData.totalResults,
       loading:false
     })
+     this.props.setProgress(100);
   }
   async componentDidMount(){
     // let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=271126b49b7c45ddaca3ee43f3937987&page=1&pageSize=${this.props.pageSize}`;
